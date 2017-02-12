96d0ea803474a54df6d5e7af12763162ce98cb50
Ashok Dudhade <ashok.dudhade@outlook.com>
2017-02-11 18:37:55 -0600

    Yaml parser changes

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,79 @@
-import * as fs from "fs";
-class YamlParser{
-    path: string;
-    constructor(path: string){
-        this.path = path;       
+/// <reference path="../typings/tsd.d.ts" />
+
+import fs = require('fs');
+import * as _ from "lodash";
+import * as yaml from "js-yaml";
+
+class YamlParser {
+
+    constructor() {
+
     }
 
-    public compare(){
-        if(!fs.exists(this.path)){
-            throw new Error("Path: '"+ this.path +"' does not exists.");
+    public lint(path: string) {
+
+        if (!fs.existsSync(path)) {
+            throw new Error("Path: '" + path + "' does not exists.");
         }
+
+        if (fs.lstatSync(path).isDirectory()) {
+            return this.fetchYamlFileNames(path);
+        }
+
+        return [];
+    }
+
+    private getFileNames(dir, filePaths = []) {
+
+        var files = fs.readdirSync(dir);
+
+        _.forEach(files, (item) => {
+            var path = dir + "/" + item;
+            var stat = fs.lstatSync(path);
+
+            if (stat.isSymbolicLink()) {
+                return [];
+            } else if (stat.isDirectory()) {
+                this.getFileNames(path, filePaths)
+            }
+            var rePattern = new RegExp(/\.ya?ml$/);
+            var arrMatches = path.match(rePattern);
+            var isYaml = rePattern.test(path);
+            if (isYaml) {
+                filePaths.push(path);
+            }
+        });
+
+        return filePaths;
+
+    }
+
+    private fetchYamlFileNames(dirName) {
+
+        var processedFiles = [];
+        var files = this.getFileNames(dirName);
+        _.forEach(files, (item) => {
+
+
+            var fileResult = { hasError: false, message: undefined, filename: item };
+
+            var hasError = false;
+            var message;
+            try {
+                yaml.safeLoad(fs.readFileSync(item, 'utf-8'));
+            } catch (ex) {
+
+                fileResult.hasError = true;
+                fileResult.message = ex.toString();
+            }
+            
+            processedFiles.push(fileResult);
+        });
+
+        return processedFiles;
+        
     }
 }
 
-new YamlParser("test");
+var y = new YamlParser();
+y.lint('/Users/ashokdudhade/MVP/tech-ui/ui/web/config');
--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -1,1 +1,3 @@
 /// <reference path="node/node.d.ts" />
+/// <reference path="modules/lodash/index.d.ts" />
+/// <reference path="../node_modules/@types/js-yaml/index.d.ts" />
