b5c0dbb803876efbb6f59bc315f609124f35b6e6
Ashok Dudhade <ashok.dudhade@outlook.com>
2017-02-16 22:00:23 -0600

    Added tsd install

--- test.sh
+++ test.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
+tsd install -g;
+typings install -g;
 tsd install;
-typings install;
 gulp build
 istanbul cover ./node_modules/mocha/bin/_mocha --report lcovonly -- -R spec && cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js && rm -rf ./coverage
