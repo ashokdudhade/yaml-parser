dcf22d2bbb854a849dbf92ddd75053284c20fd05
Ashok Dudhade <ashok.dudhade@outlook.com>
2017-02-16 22:28:28 -0600

    Added typing install for lodash

--- test.sh
+++ test.sh
@@ -2,5 +2,6 @@
 tsd install -g;
 typings install -g;
 tsd install;
+typings install lodash;
 gulp build
 istanbul cover ./node_modules/mocha/bin/_mocha --report lcovonly -- -R spec && cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js && rm -rf ./coverage
