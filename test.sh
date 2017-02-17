5175b9b4ef1a6a99f64a1828ead260fd813a1a8f
Ashok Dudhade <ashok.dudhade@outlook.com>
2017-02-16 22:38:50 -0600

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ typings install -g;
 npm install;
 npm install ts-node;
 tsd install;
-
 typings install lodash;
+tsc;
 gulp build
 istanbul cover ./node_modules/mocha/bin/_mocha --report lcovonly -- -R spec && cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js && rm -rf ./coverage
